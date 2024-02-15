206190e6f02944e298c6a1c0bdf4aad69016b36d
Hexodust <PGTanase@gmail.com>
2024-02-16 00:46:51 +0200

    Cleanup

--- src/main/java/org/example/Current.java
+++ src/main/java/org/example/Current.java
@@ -5,16 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Current {
     private float temp_c;
-
-//    float getTemp_c(){
-//        return temp_c;
-//    }
-
-//    void setTemp_c(float temp_c){
-//        this.temp_c = temp_c;
-//    }
 }
